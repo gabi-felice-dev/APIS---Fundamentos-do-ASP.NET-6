4f0063572459268ca4a7c0ac2ffa029a3f26ad58
Gabriele Felice <gabriele_felice@hotmail.com>
2022-11-20 12:04:45 -0300

    Aula 01

--- MinhaApi/Program.cs
+++ MinhaApi/Program.cs
@@ -1,6 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+var builder = WebApplication.CreateBuilder(args); //cria var para construir app web
+var app = builder.Build(); //construir app web
 
-app.MapGet("/", () => "Hello World!");
+//VERBOS
+// GET - obter
+// POST - Enviar
+// PUT - Atualizar
+// DELETE  - Excluir
 
-app.Run();
+app.MapGet("/", () => 
+{
+    return Results.Ok("Hello World");
+});
+
+app.MapGet("/{nome}", (string nome) => {
+
+    return Results.Ok($"Hello World {nome}.");
+});
+
+app.MapPost("/", (User user) =>
+{
+    return Results.Ok(user);
+});
+
+app.Run(); //roda app web
+
+public class User
+{
+    public int Id { get; set; }  
+    public string? UserName { get; set; }
+}
+
