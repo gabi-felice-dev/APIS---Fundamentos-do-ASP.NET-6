414a25ddc34dbc35f27256e012e3dd8df1943cfa
Gabriele Felice <gabriele_felice@hotmail.com>
2022-12-04 09:05:51 -0300

    Aula 03

--- TodoMVC/Controllers/HomeController.cs
+++ TodoMVC/Controllers/HomeController.cs
@@ -30,5 +30,39 @@ namespace TodoMVC.Controllers
         {
             return context.Todos.FirstOrDefault(x => x.Id == id);
         }
+
+        [HttpPut("/{id:int}")]
+        public IActionResult Put(
+            [FromRoute] int id,
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+                return NotFound();
+
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+            return Ok(model);
+        }
+
+        [HttpDelete("/{id:int}")]
+        public IActionResult Delete(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+                return NotFound();
+
+            context.Todos.Remove(model);
+            context.SaveChanges();
+
+            return Ok(model);
+        }
+
    } 
 }
